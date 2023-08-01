3c80200044ae8bd5ef30d3e14e250806cb3d3d93
Agustin Rolando <138535452+Agustin144@users.noreply.github.com>
2023-08-01 15:10:11 +0000

    generador nuevo

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 let pronoun = ["the", "our"];
 let adj = ["great", "big"];
 let noun = ["jogger", "racoon"];
-let last = [".com"];
+let last = [".com", ".org"];
 
 for (let i = 0; i < pronoun.length; i++) {
   for (let j = 0; j < adj.length; j++) {
     for (let l = 0; l < noun.length; l++) {
-      console.log(pronoun[i] + adj[j] + noun[l] + last);
+      for (let o = 0; o < last.length; o++) {
+        console.log(pronoun[i] + adj[j] + noun[l] + last[o]);
+      }
     }
   }
 }
